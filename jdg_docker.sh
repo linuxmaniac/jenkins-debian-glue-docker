c5419df51f62e03b8ca4b2ff912b1dc8fc440969
Victor Seva <vseva@sipwise.com>
2015-08-22 17:19:36 +0200

    make helper script more flexible

--- jdg_docker.sh
+++ jdg_docker.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 ACTION="${1:-run}"
+BASE_DIR="${HOME}/Projects"
+JDB_TAG=linuxmaniac/jenkins-debian-glue-docker
+JDB_NAME=jdb
+JDB_PORT=8080
+JDB_SLAVE_PORT=50000
 
-case $ACTION in
+# volume dirs
+for dir in jdg_home jdg_pbuilder jdg_repository ; do
+  mkdir -p "${BASE_DIR}/${dir}"
+done
+
+case ${ACTION} in
   build)
-    docker build --tag linuxmaniac/jenkins-debian-glue-docker:latest .
+    docker build --tag ${JDB_TAG}:latest .
   ;;
   run)
-    docker run -i --privileged --rm=true --name jdg -p 8080:8080 -p 50000:50000 \
-      -v "${HOME}"/Projects/jdg_home:/var/jenkins_home:rw \
-      -v "${HOME}"/Projects/jdg_pbuilder:/var/cache/pbuilder/:rw \
-      -v "${HOME}"/Projects/jdg_repository:/var/repository/:rw \
-      linuxmaniac/jenkins-debian-glue-docker
+    docker run -i --privileged --rm=true --name="${JDB_NAME}" \
+      -p ${JDB_PORT}:8080 \
+      -p ${JDB_SLAVE_PORT}:50000 \
+      -v "${BASE_DIR}"/jdg_home:/var/jenkins_home:rw \
+      -v "${BASE_DIR}"/jdg_pbuilder:/var/cache/pbuilder/:rw \
+      -v "${BASE_DIR}"/jdg_repository:/var/repository/:rw "${JDB_TAG}"
   ;;
+  *)
+    echo "unknown action ${ACTION}"
+    exit 1
 esac
 
