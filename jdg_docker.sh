374239d30fe481445e1e46eff09a57daf2df271b
Victor Seva <vseva@sipwise.com>
2015-08-22 09:30:30 +0200

    pbuilder needs privileged to mount /proc add /var/repository volumen

--- jdg_docker.sh
+++ jdg_docker.sh
@@ -6,9 +6,10 @@ case $ACTION in
     docker build --tag linuxmaniac/jenkins-debian-glue-docker:latest .
   ;;
   run)
-    docker run -i --rm=true --name jdg -p 8080:8080 -p 50000:50000 \
+    docker run -i --privileged --rm=true --name jdg -p 8080:8080 -p 50000:50000 \
       -v "${HOME}"/Projects/jdg_home:/var/jenkins_home:rw \
-      -v "${HOME}"/Projects/jdg_repository:/var/cache/pbuilder/:rw \
+      -v "${HOME}"/Projects/jdg_pbuilder:/var/cache/pbuilder/:rw \
+      -v "${HOME}"/Projects/jdg_repository:/var/repository/:rw \
       linuxmaniac/jenkins-debian-glue-docker
   ;;
 esac
